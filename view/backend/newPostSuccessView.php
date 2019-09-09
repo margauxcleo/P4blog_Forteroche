4502701f7f43b155dd16db4f6e636ead1b32f82f
Margaux Vitez <margaux.vitez@hotmail.fr>
2019-09-09 11:50:04 +0200

    ajout de la vue confirmation creation nouveau post

---
+++ view/backend/newPostSuccessView.php
@@ -0,0 +1,26 @@
+<?php ob_start(); ?>
+
+<section class="bloc_page2 container-fluid">
+	
+	<div class="row">
+      
+	    <?php require("backAside.php") ?>
+
+	    <div class="admin col-xs-12 col-sm-12 col-md-8  col-lg-9 col-xl-10">
+
+	      	<div class="row">
+        		<div class="col-12 text-center">
+  		          	<h2>Créer un nouveau chapitre</h2>
+	    		</div>
+    		</div>
+
+    		<p class="alert alert-success" role="alert"> Le chapitre a été posté avec succès !        
+            </p>
+	  		
+		</div>
+	</div>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('backTemplate.php'); ?>
