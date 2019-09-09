de65b65709584b0afc7c0a34811fd5590ae1fb4e
Margaux Vitez <margaux.vitez@hotmail.fr>
2019-09-09 11:42:01 +0200

    ajout de la vue confirmation suppression comment

---
+++ view/backend/deleteCommentSuccessView.php
@@ -0,0 +1,26 @@
+<?php ob_start(); ?>
+
+<section class="bloc_page2 container-fluid">
+	
+	<div class="row">
+      
+	    <?php require("backAside.php") ?>
+
+	    <div class="admin col-xs-12 col-sm-12 col-md-8  col-lg-9 col-xl-10">
+
+	      	<div class="row">
+        		<div class="col-12 text-center">
+  		          	<h2>Supprimer un commentaire</h2>
+	    		</div>
+    		</div>
+
+    		<p class="alert alert-success" role="alert"> Le commentaire a été supprimé avec succès !        
+            </p>
+	  		
+		</div>
+	</div>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('backTemplate.php'); ?>
