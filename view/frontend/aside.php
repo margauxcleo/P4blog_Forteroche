46750cd6a91194d1b3b285a8f68aa9f8485860c3
Margaux Vitez <margaux.vitez@hotmail.fr>
2019-09-17 15:45:19 +0200

    modif des classes du button de l'aside pour correction menu collapse version mobile

--- view/frontend/aside.php
+++ view/frontend/aside.php
@@ -2,7 +2,7 @@
 
 
   <nav class="navbar navbar-light bg-light navbar-expand-md">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#aside-navbar" aria-controls="aside-navbar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
       <span>Voir tous les chapitres</span>
     </button>
