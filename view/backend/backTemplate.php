6634e68a62abc6abfe23a74d651cd66eea559d3a
Margaux <margaux.vitez@hotmail.fr>
2019-09-18 11:59:49 +0200

    modification de la feuille de style css à cause des paramatres auto de Tiny. Modification de Tiny

--- view/backend/backTemplate.php
+++ view/backend/backTemplate.php
@@ -21,7 +21,6 @@
     tinymce.init({
         selector: '.mytinytextarea',
         language: 'fr_FR',
-        font_formats: 'bellota_regular=arial,open_sansregular,sans-serif',
         force_br_newlines: true,
         menubar: false,
         toolbar: 'insertfile undo redo | bold italic | bullist numlist outdent indent'
